9cdf071ed45dde047696cfafc05ffcc10ea911e3
beccaelenzil <beccaelenzil@gmail.com>
2019-11-25 11:15:18 -0800

    made student collection a class component

--- src/components/StudentCollection.js
+++ src/components/StudentCollection.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import Student from './Student';
 
-const StudentCollection = (props) => {
+//const StudentCollection = (props) => {
+class StudentCollection extends React.Component{
 
-  const studentCollection = props.people.map((student, i) => {
-      return <Student name={student.name} email={student.email} present={student.present} class={student.class} key={i} />;
-    }
+  makeCollection(){
+    const studentCollection = this.props.people.map((student, i) => {
+        return <Student name={student.name} email={student.email} present={student.present} class={student.class} key={i} />;
+      }
+    );
+    return studentCollection
+  }
+  render(){
+    return(
+      <ul>
+        {this.makeCollection()}
+      </ul>
   );
-
-  return(
-    <ul>
-      {studentCollection}
-    </ul>
-  );
+  }
 };
+
 
 export default StudentCollection;
