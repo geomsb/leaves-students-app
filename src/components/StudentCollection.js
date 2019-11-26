36ebb94ba3a7239fce722a244e248f8a24c3febd
CheezItMan <chris@adadevelopersacademy.org>
2019-11-26 10:06:30 -0800

    update StudentCollection to keep state for studentlist

--- src/components/StudentCollection.js
+++ src/components/StudentCollection.js
@@ -1,23 +1,63 @@
 import React from 'react';
+import PropType from 'prop-types';
 import Student from './Student';
 
 //const StudentCollection = (props) => {
-class StudentCollection extends React.Component{
+class StudentCollection extends React.Component {
+  constructor(props) {
+    super(props);
 
-  makeCollection(){
-    const studentCollection = this.props.people.map((student, i) => {
-        return <Student name={student.name} email={student.email} present={student.present} class={student.class} key={i} />;
-      }
+    this.state = {
+      students: props.people,
+    }
+  }
+
+  togglePresent = (studentEmail) => {
+    console.log('Toggling', studentEmail);
+    const { students } = this.state;
+    //    const students = this.state.students;
+
+    const studentToUpdate = students.find((student) => student.email === studentEmail);
+
+    studentToUpdate.present = !studentToUpdate.present;
+
+    this.setState({
+      //students: students,
+      students,
+    });
+
+  }
+
+  makeCollection () {
+    const studentCollection = this.state.students.map((student, i) => {
+      return <Student
+        name={student.name}
+        email={student.email}
+        present={student.present}
+        class={student.class}
+        togglePresent={this.togglePresent}
+        key={i}
+      />;
+    }
     );
     return studentCollection
   }
-  render(){
-    return(
+  render () {
+    return (
       <ul>
         {this.makeCollection()}
       </ul>
-  );
+    );
   }
+};
+
+StudentCollection.propTypes = {
+  people: PropType.arrayOf(PropType.shape({
+    name: PropType.string.isRequired,
+    email: PropType.string.isRequired,
+    class: PropType.string,
+    present: PropType.bool,
+  })),
 };
 
 
