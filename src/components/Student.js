8d35c9e9885604c86b6a51df9fc662f32b0dda04
CheezItMan <chris@adadevelopersacademy.org>
2019-11-25 20:55:19 -0800

    update student component to functional and add event handlers

--- src/components/Student.js
+++ src/components/Student.js
@@ -3,39 +3,42 @@ import './Student.css';
 import PropTypes from 'prop-types';
 
 
-class Student extends React.Component {
-  //const Student = (props) => {
-  // Component functions always return JSX
-
-  constructor(props) {
-    super(props) //this.props = props
-    this.state = { present: false }
-  }
-
-  buildStyles () {
-    let selectStyles = this.props.class.toLowerCase();
-    selectStyles = selectStyles + ' ' + (this.props.present ? 'present' : 'absent');
+const Student = (props) => {
+  const buildStyles = () => {
+    let selectStyles = props.class.toLowerCase();
+    selectStyles = selectStyles + ' ' + (props.present ? 'present' : 'absent');
     return selectStyles;
   }
 
-  render () {
-    return (
-      <li className={this.buildStyles()}>
-        {this.props.name} <p>{this.props.email}</p>
-        <button>
-          Mark {this.props.present ? "Absent" : "Present"}
-        </button>
-      </li>
-    );
+  const onTogglePresentClick = () => {
+    console.log('Toggle Present Click', props);
+    props.onTogglePresent(props.id);
   }
+
+  const onDeleteClick = () => {
+    props.onDeleteStudent(props.id);
+  }
+
+  return (
+    <li className={buildStyles()}>
+      {props.name} <p>{props.email}</p>
+      <button onClick={onTogglePresentClick}>
+        Mark {props.present ? "Absent" : "Present"}
+      </button>
+      <button onClick={onDeleteClick}>Delete</button>
+    </li>
+  );
 };
 
 
 Student.propTypes = {
+  id: PropTypes.number.isRequired,
   name: PropTypes.string.isRequired,
   email: PropTypes.string,
   class: PropTypes.string,
-  present: PropTypes.bool.isRequired
+  present: PropTypes.bool.isRequired,
+  onDeleteStudent: PropTypes.func.isRequired,
+  onTogglePresent: PropTypes.func.isRequired,
 };
 
 Student.defaultProps = {
